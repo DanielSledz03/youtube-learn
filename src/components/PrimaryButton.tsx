d05014563e9a94acb5935a8185d527e715883129
DanielSledz03 <daniel.sledz@pluscode.io>
2024-08-31 18:56:42 +0200

    init: primary button

---
+++ src/components/PrimaryButton.tsx
@@ -0,0 +1,37 @@
+import { StyleSheet, TouchableOpacity } from "react-native";
+import PoppinsText from "./PoppinsText";
+import colors from "../constants/colors";
+
+interface PrimaryButtonProps {
+  title: string;
+  onPress: () => void;
+}
+
+const PrimaryButton = ({ title, ...props }: PrimaryButtonProps) => {
+  return (
+    <TouchableOpacity style={styles.container} {...props}>
+      <PoppinsText style={styles.text} weight="SemiBold">
+        {title}
+      </PoppinsText>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    minHeight: 50,
+    backgroundColor: colors.darkBlue,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 12,
+    marginVertical: 30,
+  },
+
+  text: {
+    color: colors.white,
+    fontSize: 16,
+  },
+});
+
+export default PrimaryButton;
