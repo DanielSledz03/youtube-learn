32c0e61b3427c3105d49f9bef20624890299b409
DanielSledz03 <daniel.sledz@pluscode.io>
2024-08-31 16:45:12 +0200

    init: poppins font and component

---
+++ src/components/PoppinsText.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Text, TextProps } from "react-native";
+
+interface PoppinsTextProps extends TextProps {
+  weight:
+    | "Black"
+    | "BlackItalic"
+    | "Bold"
+    | "BoldItalic"
+    | "ExtraBold"
+    | "ExtraBoldItalic"
+    | "ExtraLight"
+    | "ExtraLightItalic"
+    | "Italic"
+    | "Light"
+    | "LightItalic"
+    | "Medium"
+    | "MediumItalic"
+    | "Regular"
+    | "SemiBold"
+    | "SemiBoldItalic"
+    | "Thin"
+    | "ThinItalic";
+}
+
+const PoppinsText: React.FC<PoppinsTextProps> = ({
+  style,
+  weight,
+  ...props
+}) => {
+  return (
+    <Text style={[style, { fontFamily: `Poppins-${weight}` }]} {...props} />
+  );
+};
+
+export default PoppinsText;
